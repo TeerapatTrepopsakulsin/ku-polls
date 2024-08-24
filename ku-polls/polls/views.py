62d75c0c1cd453ae960658b87cd0fb76480cc9ed
TeerapatTREPOPSAKULSIN <teerapat.tre@ku.th>
2024-08-24 17:14:08 +0700

    Raising a 404 error when occured

--- ku-polls/polls/views.py
+++ ku-polls/polls/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponse, Http404
 from django.template import loader
 
 from .models import Question
@@ -11,18 +11,9 @@ def index(request):
     return render(request, "polls/index.html", context)
 
 
-# Alternative
-# def index(request):
-#     latest_question_list = Question.objects.order_by("-pub_date")[:5]
-#     template = loader.get_template("polls/index.html")
-#     context = {
-#         "latest_question_list": latest_question_list,
-#     }
-#     return HttpResponse(template.render(context, request))
-
-
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/detail.html", {"question": question})
 
 
 def results(request, question_id):
